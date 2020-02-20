b5f472014a3c398e6924d3cfe7abf5507e58fde1
shotakeu <shotakeu.work@gmail.com>
2020-02-20 02:22:05 +0000

    6-2 end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
+  
+  rescue_from StandardError, with: :rescue500
 
   private def set_layout
     if params[:controller].match(%r{\A(staff|admin|customer)/})
@@ -7,5 +9,9 @@ class ApplicationController < ActionController::Base
     else
       "customer"
     end
+  end
+
+  private def rescue500(e)
+    render "errors/internal_server_error", status: 500
   end
 end
