f360e38cc4bfd09cb5857e8db267ca1a481cc4fe
shotakeu <shotakeu.work@gmail.com>
2020-02-24 02:19:14 +0000

    chapter 9-2-3 end

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,7 @@ Rails.application.routes.draw do
     get "login" => "sessions#new", as: :login
     post "session" => "sessions#create", as: :session
     delete "session" => "sessions#destroy"
-    get "staff_members" => "staff_members#index"
-    get "staff_members/:id" => "staff_members#show"
-    get "staff_members/new" => "staff_members#new"
-    get "staff_members/:id/edit" => "staff_members#edit"
-    post "staff_members" => "staff_members#create"
-    patch "staff_members/:id" => "staff_members#update"
-    delete "staff_members/:id" => "staff_members#destroy"
+    resources :staff_members
   end
 
   namespace :customer do
