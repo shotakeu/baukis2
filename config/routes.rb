afd7d4a12633840bf7aac641c5fdac30594bd8ee
shotakeu <shotakeu.work@gmail.com>
2020-02-24 02:08:10 +0000

    chapter 9-1 end

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :staff do
+  namespace :staff, path: ""  do
     root "top#index"
     get "login" => "sessions#new", as: :login
     post "session" => "sessions#create", as: :session
@@ -8,9 +8,9 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root "top#index"
-    get "login" => "session#new", as: :login
-    post "session" => "session#create", as: :session
-    delete "session" => "session#destroy"
+    get "login" => "sessions#new", as: :login
+    post "session" => "sessions#create", as: :session
+    delete "session" => "sessions#destroy"
   end
 
   namespace :customer do
