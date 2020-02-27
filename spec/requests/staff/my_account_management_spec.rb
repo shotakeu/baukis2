a43e9a55cf5b700853eeb3a309d9985875da558a
shotakeu <shotakeu.work@gmail.com>
2020-02-27 00:02:36 +0000

    chapter 12-4 end

--- spec/requests/staff/my_account_management_spec.rb
+++ spec/requests/staff/my_account_management_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-describe "職員による自分のアカウント管理", "ログイン前" do
+describe "職員による自分のアカウントの管理", "ログイン前" do
   include_examples "a protected singular staff controller", "staff/accounts"
 end
 
@@ -13,32 +13,6 @@ describe "職員による自分のアカウントの管理" do
           password: "pw"
         }
       }
-  end
-
-  describe "更新" do
-    let(:params_hash) { attributes_for(:staff_member) }
-    let(:staff_member) { create(:staff_member) }
-
-    example "email属性を変更する" do
-      params_hash.merge!(email: "test@example.com")
-      patch staff_account_url,
-        params: { id: staff_member.id, staff_member: params_hash }
-      staff_member.reload
-      expect(staff_member.email).to eq("test@example.com")
-    end
-
-    example "例外ActionContoller::ParameterMissingが発生" do
-      expect { patch staff_account_url, params: { id: staff_member.id } }.
-      to raise_error(ActionController::ParameterMissing)
-    end
-
-    example "end_dateの値の書き換え不可" do
-      params_hash.merge!(end_date: Date.tomorrow)
-      expect {
-        patch staff_account_url,
-          params: { id: staff_member.id, staff_member: params_hash }
-      }.not_to change { staff_member.end_date }
-    end
   end
 
   describe "情報表示" do
@@ -54,11 +28,37 @@ describe "職員による自分のアカウントの管理" do
       get staff_account_url
       expect(response).to redirect_to(staff_root_url)
     end
+
+    example "セッションタイムアウト" do
+      travel_to Staff::Base::TIMEOUT.from_now.advance(seconds: 1)
+      get staff_account_url
+      expect(response).to redirect_to(staff_login_url)
+    end
   end
 
-  example "セッションタイムアウト" do
-    travel_to Staff::Base::TIMEOUT.from_now.advance(seconds: 1)
-    get staff_account_url
-    expect(response).to redirect_to(staff_login_url) #fixme : staff_login_urlなんてあるのか？
+  describe "更新" do
+    let(:params_hash) { attributes_for(:staff_member) }
+    let(:staff_member) { create(:staff_member) }
+
+    example "email属性を変更する" do
+      params_hash.merge!(email: "test@example.com")
+      patch staff_account_url,
+        params: { id: staff_member.id, staff_member: params_hash }
+      staff_member.reload
+      expect(staff_member.email).to eq("test@example.com")
+    end
+
+    example "例外ActionController::ParameterMissingが発生" do
+      expect { patch staff_account_url, params: { id: staff_member.id } }.
+        to raise_error(ActionController::ParameterMissing)
+    end
+
+    example "end_dateの値は書き換え不可" do
+      params_hash.merge!(end_date: Date.tomorrow)
+      expect {
+        patch staff_account_url,
+          params: { id: staff_member.id, staff_member: params_hash }
+      }.not_to change { staff_member.end_date }
+    end
   end
 end
