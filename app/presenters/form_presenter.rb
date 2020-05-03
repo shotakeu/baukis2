902b8ddd38849887c82d4aed5ef948204e078ae8
shotakeu <shotakeu.work@gmail.com>
2020-05-03 03:35:44 +0000

    fix error

--- app/presenters/form_presenter.rb
+++ app/presenters/form_presenter.rb
@@ -2,7 +2,7 @@ class FormPresenter
   include HtmlBuilder
 
   attr_reader :form_builder, :view_context
-  delegate :label, :text_field, :date_field, :password_feild,
+  delegate :label, :text_field, :date_field, :password_field,
     :check_box, :radio_button, :text_area, :object, to: :form_builder
 
   def initialize(form_builder, view_context)
